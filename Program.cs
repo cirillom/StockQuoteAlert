ea3597722f42266d865a7024949d50f70fb90287
Matheus Cirillo <matheuscirillo@usp.br>
2026-03-17 12:26:27 -0300

    Readin variables

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿if (args.Length == 3)
+{
+
+    string stockName = args[0];
+    string sellPriceStr = args[1];
+    string buyPriceStr = args[2];
+
+    int sellPriceCents = (int)(decimal.Parse(sellPriceStr) * 100);
+    int buyPriceCents = (int)(decimal.Parse(buyPriceStr) * 100);
+
+    Console.WriteLine($"Stock: {stockName}");
+    Console.WriteLine($"Sell Price: R${sellPriceCents/100.0}");
+    Console.WriteLine($"Buy Price: R${buyPriceCents/100.0}");
+}
+else
+{
+    Console.WriteLine("No command-line arguments string provided.");
+}
