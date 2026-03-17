a3c72ca63e8e692324d5f1d46ecf6f921c8af77e
Matheus Cirillo <matheuscirillo@usp.br>
2026-03-17 13:38:03 -0300

    Stock class and simple stock simulation

--- Program.cs
+++ Program.cs
@@ -1,18 +1,56 @@
-﻿if (args.Length == 3)
+﻿using StockQuoteAlert;
+
+if (args.Length != 3)
 {
-
-    string stockName = args[0];
-    string sellPriceStr = args[1];
-    string buyPriceStr = args[2];
-
-    int sellPriceCents = (int)(decimal.Parse(sellPriceStr) * 100);
-    int buyPriceCents = (int)(decimal.Parse(buyPriceStr) * 100);
-
-    Console.WriteLine($"Stock: {stockName}");
-    Console.WriteLine($"Sell Price: R${sellPriceCents/100.0}");
-    Console.WriteLine($"Buy Price: R${buyPriceCents/100.0}");
+    Console.WriteLine("Please provide the stock name, sell price, and buy price as command-line arguments.");
+    Console.WriteLine("Usage: .\\StockQuotAlert <StockName> <SellPrice> <BuyPrice>");
+    return;
 }
-else
+
+string stockName = args[0];
+string sellPriceStr = args[1];
+string buyPriceStr = args[2];
+
+int sellPriceCents = (int)(decimal.Parse(sellPriceStr) * 100);
+int buyPriceCents = (int)(decimal.Parse(buyPriceStr) * 100);
+
+Console.WriteLine($"Stock: {stockName}");
+Console.WriteLine($"Sell Price: R${sellPriceCents/100.0}");
+Console.WriteLine($"Buy Price: R${buyPriceCents/100.0}");
+
+Stock stock = new Stock(stockName, sellPriceCents, buyPriceCents);
+
+// loop to continuously check the stock price and print the recommendation
+bool isRunning = true;
+
+Console.CancelKeyPress += (sender, e) =>
 {
-    Console.WriteLine("No command-line arguments string provided.");
+    Console.WriteLine("\nStopping monitor...");
+    e.Cancel = true;
+    isRunning = false;
+};
+
+Console.WriteLine("Starting to monitor stock price. Press Ctrl+C to stop.");
+
+while (isRunning)
+{
+    var recommendation = stock.GetRecommentation();
+    switch (recommendation)
+    {
+        case RecommendationState.Buy:
+            Console.ForegroundColor = ConsoleColor.Green;
+            //send email to user saying to Buy the stock
+            break;
+        case RecommendationState.Sell:
+            //send email to user saying to Sell the stock
+            Console.ForegroundColor = ConsoleColor.Red;
+            break;
+        default:
+            break;
+    }
+    Console.WriteLine($"Current recommendation for {stock.Name}: {recommendation}");
+    Console.ResetColor();
+
+
+    await Task.Delay(1000);
 }
---
+++ Stock.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockQuoteAlert
+{
+    public enum RecommendationState
+    {
+        Buy,
+        Sell,
+        Nothing
+    }
+
+    internal class Stock
+    {
+        private string name;
+        private int sellPriceCents;
+        private int buyPriceCents;
+        private int currentPriceCents;
+        private RecommendationState recommendation;
+
+        public Stock(string name, int sellPriceCents, int buyPriceCents)
+        {
+            this.Name = name;
+            this.sellPriceCents = sellPriceCents;
+            this.buyPriceCents = buyPriceCents;
+            this.currentPriceCents = 2257;//UpdateCurrentPrice();
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                // Validate that the name is not null or empty
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Stock name cannot be null or empty.");
+                }
+
+                // TODO: Check if this is stock name exist inside B3
+                name = value;
+            }
+        }
+
+        public void UpdateCurrentPrice()
+        {
+            /*string currentPriceStr;
+            // TODO: Implement logic to fetch the current price from an API or data source
+            currentPriceStr = "22.65"; // Placeholder value for demonstration
+
+            this.currentPriceCents = (int)(decimal.Parse(currentPriceStr) * 100);
+            */
+            this.currentPriceCents += 1; // Placeholder logic to simulate price changes for demonstration
+            Console.WriteLine($"Updated current price for {Name}: R${currentPriceCents / 100.0}");
+        }
+
+        public RecommendationState GetRecommentation()
+        {
+            UpdateCurrentPrice();
+
+            if (this.currentPriceCents > this.sellPriceCents)
+            {
+                return RecommendationState.Sell;
+
+            }
+            else if (this.currentPriceCents < this.buyPriceCents)
+            {
+                return RecommendationState.Buy;
+            }
+
+            return RecommendationState.Nothing;
+        }
+    }
+}
